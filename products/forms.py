4dd6c355699f4f625d569c98bd2777c887cd5a6d
HectorAVJr <hectoravjr@outlook.com>
2026-04-22 11:24:50 -0500

    Create ProductForm for managing product discounts
    Added ProductForm to handle product discount fields with date inputs. #64

--- products/forms.py
+++ products/forms.py
@@ -1,1 +1,15 @@
 from sellers.forms import *
+
+from sellers.forms import *
+from django import forms
+from .models import Product
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['discount_percent', 'discount_start_date', 'discount_end_date']
+        widgets = {
+            'discount_start_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'discount_end_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
