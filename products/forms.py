7e9e81d7159a32b50891c9459af542d9d885cad8
loydemily6373 <loydemily6373@gmail.com>
2026-04-22 15:13:00 -0500

    Remove duplicate import statement in forms.py

--- products/forms.py
+++ products/forms.py
@@ -1,15 +1,1 @@
 from sellers.forms import *
-
-from sellers.forms import *
-from django import forms
-from .models import Product
-
-
-class ProductForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields = '__all__'
-        widgets = {
-            'discount_start_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
-            'discount_end_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
-        }
