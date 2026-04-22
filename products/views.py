c83e3383842f1dcca49d8c8c038142161fcf3e72
HectorAVJr <hectoravjr@outlook.com>
2026-04-22 11:25:28 -0500

    Implement create_product view for product creation
    Added create_product view to handle product creation. #64

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from sellers.views import *
 
-
+def create_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.seller = request.user
+            product.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+        
+    return render(request, 'products/create_product.html', {'form': form})
