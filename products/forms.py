93bf37e5b235341fa016386aa9b0d135e6d25106
HectorAVJr <hectoravjr@outlook.com>
2026-04-22 11:43:55 -0500

    Change ProductForm to include all fields
    #64

--- products/forms.py
+++ products/forms.py
@@ -8,7 +8,7 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['discount_percent', 'discount_start_date', 'discount_end_date']
+        fields = '__all__'
         widgets = {
             'discount_start_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
             'discount_end_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
